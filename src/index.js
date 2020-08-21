cfa8be65231ee76f1e8f1d13997fd00e30e42ee0
manggic <manishmahto198@gmail.com>
2020-08-21 16:34:04 +0530

    calcreact

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App  name= "manish"  />
   </React.StrictMode>,
   document.getElementById('root')
 );
